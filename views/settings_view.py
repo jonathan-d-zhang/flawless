3e65cb55a403b15714bced4667a7c5acf0b7ffee
EwwPhysics <sashakmurray@gmail.com>
2021-03-29 14:52:01 -0400

    Add setting slider and center settings on screen.
    Settings now span middle half of screen.
    Full screen no longer messes it up

--- views/settings_view.py
+++ views/settings_view.py
@@ -11,7 +11,7 @@ class SettingsView(arcade.View):
     def __init__(self):
         super().__init__()
         self.ui_manager = arcade.gui.UIManager()
-        self.last_size = (width, height) = self.window.get_size()
+        self.last_size = (self.width, self.height) = self.window.get_size()
         # use setting_index to grab the currently selected setting
         self.setting_index = 0
 
@@ -19,10 +19,11 @@ class SettingsView(arcade.View):
         self.setting_list = [
             partial(SettingToggle, text="Turn music off/on", binding="is_music_on"),
             partial(SettingToggle, text="Fullscreen", binding="is_fullscreen"),
-            partial(SettingSlider, text="Adjust volume", binding="is_fullscreen"),
+            partial(SettingSlider, text="Adjust volume", binding="music_volume"),
+            partial(SettingToggle, text="test", binding="is_music_on")
         ]
         self.setting_list = [
-            setting(width // 2 - 25, height - i * 50 - height // 8)
+            setting(self.width // 4, self.height - i * 60 - self.height // 8)
             for i, setting in enumerate(self.setting_list)
         ]
 
@@ -30,25 +31,36 @@ class SettingsView(arcade.View):
         arcade.start_render()
         self.setup()
 
-        longest = 0
-        for setting in self.setting_list:
-            if setting.length > longest:
-                longest = setting.length
+        # longest = 0
+        # for setting in self.setting_list:
+        #     if setting.length > longest:
+        #         longest = setting.length
 
+        longest = (self.width // 2)
         for setting in self.setting_list:
             setting.draw(longest)
 
         setting = self.setting_list[self.setting_index]
         x = setting.x + (longest + 60) // 2
-        y = setting.center_y
-        width = longest + 120
-        arcade.draw_rectangle_outline(
-            center_x=x,
-            center_y=y,
-            width=width,
-            height=30,
-            color=arcade.color.WHITE,
-        )
+        width = longest + 100
+        if type(setting) == SettingToggle:
+            arcade.draw_rectangle_outline(
+                center_x=x,
+                center_y=setting.y + 8,
+                width=width,
+                height=30,
+                color=arcade.color.WHITE,
+            )
+        else:
+            arcade.draw_rectangle_outline(
+                center_x=x,
+                center_y=setting.y,
+                width=width,
+                height=60,
+                color=arcade.color.WHITE,
+                )
+        arcade.draw_line(self.width // 2, 0, self.width // 2, 400, arcade.color.WHITE)
+
 
     def on_show_view(self):
         ...
@@ -58,10 +70,10 @@ class SettingsView(arcade.View):
 
     def update(self, delta_time: float):
         if self.last_size != (new_size := self.window.get_size()):
-            width, height = new_size
+            self.width, self.height = new_size
             for i, setting in enumerate(self.setting_list):
-                setting.x = width // 2 - 25
-                setting.y = height - i * 50 - height // 8
+                setting.x = self.width // 4
+                setting.y = self.height - i * 50 - self.height // 8
         self.last_size = new_size
 
     def on_key_press(self, symbol, modifiers):
@@ -94,9 +106,6 @@ class SettingField(ABC):
         self.text = text
         self.binding = binding
         self.length = len(self.text) * 8
-
-        self.center_x = self.x + (self.length // 2)
-        self.center_y = self.y + 8
 
     @property
     def value(self):
@@ -150,27 +159,26 @@ class SettingToggle(SettingField):
             self.y,
             color=arcade.csscolor.WHITE,
             width=self.length,
-            font_name="arial.ttf",
         )
         arcade.draw_rectangle_outline(
-            self.x + longest + 35, self.center_y, 49, 20, color=arcade.color.AQUA
+            self.x + longest + 35, self.y + 8, 49, 20, color=arcade.color.AQUA
         )
         if self.value:
             arcade.draw_rectangle_filled(
-                self.x + longest + 35,
-                self.center_y,
-                48,
+                self.x + longest + 47,
+                self.y + 8,
+                23,
                 18,
-                color=arcade.color.ARYLIDE_YELLOW,
+                color=arcade.color.AO,
             )
 
         else:
             arcade.draw_rectangle_filled(
                 self.x + longest + 23,
-                self.center_y,
+                self.y + 8,
                 23,
                 18,
-                color=arcade.color.ARYLIDE_YELLOW,
+                color=arcade.color.RED,
             )
 
 
@@ -183,9 +191,23 @@ class SettingSlider(SettingField):
         super().__init__(x, y, text, binding)
 
     def decrease(self):
-        if 1 <= self.value < 11:
+        if 2 <= self.value:
             self.value -= 1
 
     def increase(self):
-        if 1 <= self.value < 11:
+        if self.value < 10:
             self.value += 1
+
+    def draw(self, longest):
+        arcade.draw_text(
+            self.text,
+            self.x,
+            self.y,
+            color=arcade.csscolor.WHITE,
+            width=self.length,
+        )
+        arcade.draw_line(self.x, self.y - 15, self.x + longest, self.y - 15, arcade.color.WHITE)
+        arcade.draw_text(str(self.value), self.x + longest + 25, self.y - 10, arcade.color.WHITE, 20)
+
+        tick_len = longest // 9
+        arcade.draw_circle_filled(self.x + (tick_len * (self.value - 1)), self.y - 15, 8.0, arcade.color.WHITE)
