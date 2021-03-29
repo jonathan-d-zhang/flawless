4536391e89b73ffb34e72a4d6590e282b819b57f
ForbiddenKJ <jamesbutcher167@gmail.com>
2021-03-29 20:36:35 +0100

    Added Dataclasses and returning those instead of list of dict

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
-from typing import NamedTuple
+from typing import NamedTuple, Union
+from model.object_layer import Object, ObjectLayer
 
 from constants import *
 
 from xml.dom import minidom
+
+coordinate = dict[str, float]
 
 
 class Vector(NamedTuple):
@@ -33,58 +36,61 @@ def center_of_tile(x: int, y: int) -> Vector:
     )
 
 
-def process_objects(file_path: str) -> list:
+def process_objects(file_path: str) -> list[ObjectLayer]:
     """
     Reads the .tmx file that the tile infomation is stored in an process the object infomation into
     a list of dicts
     :return: list constaining a dictionary for each layer
     """
 
-    guards = []
+    object_layers: list[ObjectLayer] = []
 
     file = minidom.parse(file_path)
     objects = file.getElementsByTagName("objectgroup")
 
     for i in objects:
-        guard = {"name": i.getAttribute("name"), "objects": [], "object_count": 0}
+        object_layer = ObjectLayer(
+            name=i.getAttribute("name"), objects=[], object_count=0
+        )
 
         child_object_elements = i.getElementsByTagName("object")
 
         for x in child_object_elements:
-            guard["object_count"] += 1
-            guard["objects"].append(
-                {
-                    "name": x.getAttribute("name"),
-                    "type": x.getAttribute("type"),
-                    "x": x.getAttribute("x"),
-                    "y": x.getAttribute("y"),
-                    "width": x.getAttribute("width"),
-                    "height": x.getAttribute("width"),
-                }
+            object_layer.object_count += 1
+
+            object_infomation = Object(
+                name=x.getAttribute("name"),
+                type=x.getAttribute("type"),
+                x=float(x.getAttribute("x")),
+                y=float(x.getAttribute("y")),
+                width=float(x.getAttribute("width")),
+                height=float(x.getAttribute("height")),
             )
 
-        guards.append(guard)
+            object_layer.objects.append(object_infomation)
 
-    return guards
+        object_layers.append(object_layer)
+
+    return object_layers
 
 
-def extract_guard_locations(layer_data: dict) -> dict:
+def extract_guard_locations(layer_data: ObjectLayer,) -> dict[str, list[coordinate]]:
     """
     Extracts the infomation that can be generated from process_objects about the guards spawn location and the
     waypoints it must patrol
     :return: dictionary containing spawn (dict) and waypoints (list of dicts)
     """
 
-    locations = {"spawn": {"x": 0, "y": 0}, "waypoints": []}
+    locations = {"spawn": [{"x": 0, "y": 0}], "waypoints": []}
 
-    locations["waypoints"] = [False for i in range(layer_data["object_count"] - 1)]
+    locations["waypoints"] = [False for i in range(layer_data.object_count - 1)]
 
-    for i in layer_data["objects"]:
-        if i["type"] == "spawn":
-            locations["spawn"]["x"] = i["x"]
-            locations["spawn"]["y"] = i["y"]
+    for i in layer_data.objects:
+        if i.type == "spawn":
+            locations["spawn"]["x"] = i.x
+            locations["spawn"]["y"] = i.y
 
-        if i["type"] == "point":
-            locations["waypoints"][int(i["name"])] = {"x": i["x"], "y": i["y"]}
+        if i.type == "point":
+            locations["waypoints"][int(i.name)] = {"x": i.x, "y": i.y}
 
     return locations
