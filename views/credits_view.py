fb5ae5acbfdd73ac696e510173cb163817845aae
EwwPhysics <sashakmurray@gmail.com>
2021-03-30 10:44:10 -0400

    Add credits view
    Will add actual credits later, may have to use layout different than other views in order to fit all of them.

---
+++ views/credits_view.py
@@ -0,0 +1,36 @@
+import arcade
+
+from .menu_view import MenuView, MenuField
+
+TEXT_COLOR = arcade.csscolor.WHITE
+
+
+class CreditsView(MenuView):
+    def __init__(self, parent_view):
+        super().__init__()
+        self.parent_view = parent_view
+
+    def on_draw(self):
+        arcade.start_render()
+
+        arcade.draw_text(
+            "Credits",
+            self.width // 2,
+            self.height * 0.75,
+            TEXT_COLOR,
+            20,
+            anchor_x="center",
+        )
+
+        arcade.draw_text(
+            "Press ESC to go back",
+            self.width // 16,
+            self.height * 7 / 8,
+            arcade.color.WHITE,
+        )
+
+
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol == arcade.key.ESCAPE:
+            self.window.show_view(self.parent_view)
