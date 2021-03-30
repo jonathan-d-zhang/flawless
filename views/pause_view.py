e32bb71ec156b7e77865661328192237844d5d70
EwwPhysics <sashakmurray@gmail.com>
2021-03-29 23:08:46 -0400

    Fix full screen and use TEXT_COLOR constant

--- views/pause_view.py
+++ views/pause_view.py
@@ -5,6 +5,7 @@ from .menu_view import MenuView, MenuField
 from .settings_view import SettingsView
 from .main_menu_view import MainMenuView
 
+TEXT_COLOR = arcade.csscolor.WHITE
 
 class PauseView(MenuView):
     def __init__(self):
@@ -18,18 +19,21 @@ class PauseView(MenuView):
     def on_draw(self):
         arcade.start_render()
 
-        arcade.draw_text("Game is Paused", self.width // 2, self.height * 0.75, arcade.color.WHITE, 20, anchor_x="center")
+        arcade.draw_text("Game is Paused", self.width // 2, self.height * 0.75, TEXT_COLOR, 20, anchor_x="center")
         for field in self.field_list:
             field.draw()
 
         current_field = self.field_list[self.selection_index]
-        arcade.draw_rectangle_outline(current_field.x, current_field.y + 8, self.width // 8, 30, arcade.color.WHITE)
+        arcade.draw_rectangle_outline(current_field.x, current_field.y + 8, self.width // 8, 30, TEXT_COLOR)
+
+        t = "Up and down to navigate, ENTER to select"
+        arcade.draw_text(t, self.width // 16, self.height // 8, TEXT_COLOR)
 
     def update(self, delta_time: float):
         if (self.width, self.height) != (new_size := self.window.get_size()):
             self.width, self.height = new_size
             for i, field in enumerate(self.field_list):
-                field.x = self.width // 20
+                field.x = self.width // 2
                 field.y = self.height - i * 50 - self.height // 2
 
     def on_hide_view(self):
@@ -45,7 +49,7 @@ class PauseView(MenuView):
         elif symbol == arcade.key.ENTER:
             current_option = self.field_list[self.selection_index].text 
             if current_option == "Resume":
-                # load gameview
+                # load game view
                 # self.window.show_view(GameView)
                 pass
             elif current_option == "Settings":
@@ -59,4 +63,4 @@ class PauseField(MenuField):
         super().__init__(x, y, text)
 
     def draw(self):
-        arcade.draw_text(self.text, self.x, self.y, arcade.color.WHITE, anchor_x="center")
+        arcade.draw_text(self.text, self.x, self.y, TEXT_COLOR, anchor_x="center")
