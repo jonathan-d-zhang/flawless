545f2c8956b54e6c2b4d94cce1b8bc0efbb4dda3
GDWR <gregory.dwr@gmail.com>
2021-03-31 00:00:13 +0100

    :recycle: Refactoring into readable draw method.

--- ingame_ui.py
+++ ingame_ui.py
@@ -14,31 +14,68 @@ class IngameUI:
         self.inv_sprite = arcade.SpriteList()
         self.key_sprite = arcade.Sprite("assets/sprites/key.png", 2)
         self.inv_sprite.append(self.key_sprite)
+        self.colour = int(0x22), int(0x3D), int(0x28)
 
-    def draw(self, current_level: int, viewport: tuple[float, float, float, float]):
-        vp_left, vp_right, vp_bottom, vp_top = viewport
-        background_width, background_height = 135, 75
-        point_list = (
-            (vp_right, vp_top),
-            (vp_right - background_width, vp_top),
-            (vp_right - background_width, vp_top - background_height),
-            (vp_right, vp_top - background_width),
+    def _draw_level(self, top, right) -> tuple[int, int]:
+        """
+        :return: The xy coords for the Top Left of the containing bounding box
+        """
+        padding_top, padding_right = 5, 50
+        arcade.draw_text(
+            text="Level",
+            start_x=right - (self.key_sprite.width // 2) - padding_right,
+            start_y=top - (self.key_sprite.height // 2) - padding_top,
+            color=arcade.color.WHITE,
+            font_size=24,
         )
-        arcade.draw_polygon_filled(point_list, (int(0x22), int(0x3D), int(0x28)))
 
+        padding_top, padding_right = 45, padding_right - 20
+        arcade.draw_text(
+            text="1",
+            start_x=right - (self.key_sprite.width // 2) - padding_right,
+            start_y=top - (self.key_sprite.height // 2) - padding_top,
+            color=arcade.color.WHITE,
+            font_size=36,
+        )
+
+        return right - (self.key_sprite.width // 2) - padding_right, top
+
+    def _draw_keys(self, top, right) -> tuple[int, int]:
+        """
+        :return: The xy coords for the Top Left of the containing bounding box
+        """
         padding_top, padding_right = 10, 5
-        self.key_sprite.center_x = (
-            vp_right - (self.key_sprite.width // 2) - padding_right
-        )
-        self.key_sprite.center_y = vp_top - (self.key_sprite.height // 2) - padding_top
+        left = right - (self.key_sprite.width // 2)
+
+        self.key_sprite.center_x = left - padding_right
+        self.key_sprite.center_y = top - (self.key_sprite.height // 2) - padding_top
 
         padding_top, padding_right = 40, self.key_sprite.width + 10
         arcade.draw_text(
             text=str(self.player_inv.keys),
-            start_x=vp_right - (self.key_sprite.width // 2) - padding_right,
-            start_y=vp_top - (self.key_sprite.height // 2) - padding_top,
+            start_x=left - padding_right,
+            start_y=top - (self.key_sprite.height // 2) - padding_top,
             color=arcade.color.WHITE,
             font_size=48,
         )
+
+        return left - padding_right, top
+
+    def _draw_background(self, top, right):
+        background_width, background_height = 200, 75
+        point_list = (
+            (right, top),
+            (right - background_width, top),
+            (right - background_width, top - background_height),
+            (right, top - background_height - (background_height // 3)),
+        )
+        arcade.draw_polygon_filled(point_list, self.colour)
+
+    def draw(self, current_level: int, viewport: tuple[float, float, float, float]):
+        _, right, _, top = viewport
+
+        self._draw_background(top, right)
+        right, top = self._draw_keys(top, right)
+        right, top = self._draw_level(top, right)
 
         self.inv_sprite.draw(filter=GL_NEAREST)
