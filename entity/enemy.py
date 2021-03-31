cf9d4f5a9be31abd3dac610a8cc36caf76b5c286
anythingapplied <anythingjewish@gmail.com>
2021-03-31 12:53:42 -0500

    Enemy Movement (#33)
    * Created new logic thet allows guards to move along their path after each
    player move
    Changes:
    * Creates and draws a path for the guard to follow through all waypoints
    * Added additional guard to layer data and support for multiple guards
    * Created a tile location to arcade location mapping
    * Places guards at actual spawn points
    
    Remaining Issues:
    * Paths are drawn at top left corner instead of center
    * Paths should be different colors for each guard
    * Paths are sometimes returning None, presumably because no path was
      found, even when there should've been
    * Didn't set correct edge restrictions for a-star pathing
    * Need to implement guard turning and direction facing
    * With a-star, guards take a slightly different path back, maybe not the
      best choice. Unable to distinguish forward path from backward path so
      breaks predictability for the player
    
    * * Fixed issues with centering paths
    * Fixed issue with doubling up of wayponts in paths
    
    * Changed A* logic to simple horizontal movement and then vertical
    movement until the destination is reached.
    
    This ignores walls and other cops.  This puts more responsibility on map
    makers, but realistically they shouldn't be including diagonals anyway
    due to the ambiguity of which way the cop would go. Guard collisions
    would've lead to hard to predict behaviors.
    
    * Added rotating colors for enemy path's
    
    * Fixed bug with enemy path having double points at each waypoint
    
    Co-authored-by: Anythingapplied <anythingapplied@userdelta.com>

--- entity/enemy.py
+++ entity/enemy.py
@@ -3,27 +3,81 @@ import random
 import arcade
 
 from constants import TILE_SIZE
+from utils import Vector, center_of_tile
 
+class pathcolors:
+    pathcoloridx = 0
+    pathcolorlist = [
+        (97, 82, 103),
+        (34, 61, 40),
+        (57, 48, 51),
+        (230, 211, 179),
+        (65, 115, 76),
+        (120, 101, 128)]
+
+    @classmethod
+    def get_color(cls):
+        color = cls.pathcolorlist[cls.pathcoloridx]
+        cls.pathcoloridx += 1
+        cls.pathcoloridx %= len(cls.pathcolorlist)
+        return color
 
 class Enemy(arcade.Sprite):
-    def __init__(self, walls, *args, **kwargs):
+    def __init__(self, walls, locations, *args, **kwargs):
         super().__init__("assets/sprites/enemy.png", 1, *args, **kwargs)
         self._walls = walls
+        self.position = locations['spawn']
+        self.waypoints = locations['waypoints']
+        self.create_full_path()
+        self.pathcolor = pathcolors.get_color()
 
-    def _move_randomly(self):
-        while True:
-            original_pos = self.center_x, self.center_y
-            val = TILE_SIZE if random.randint(0, 1) else -TILE_SIZE
+    @property
+    def position(self) -> Vector:
+        return Vector(int(self.center_x), int(self.center_y))
 
-            if random.randint(0, 1):
-                self.center_x += val
-            else:
-                self.center_y += val
+    @position.setter
+    def position(self, pos):
+        self.center_x, self.center_y = map(int, pos)
 
-            if not arcade.check_for_collision_with_list(self, self._walls):
-                break
-            else:
-                self.center_x, self.center_y = original_pos
+    def calculate_path(self, waypoint1, waypoint2):
+        """
+        Generator that yields the points between waypoint1 to waypoint2
+        excludes waypoint1 from the list
+        """
+        pos_x, pos_y = waypoint1
+        while pos_x < waypoint2[0]:
+            pos_x += TILE_SIZE
+            yield pos_x, pos_y
+        while pos_x > waypoint2[0]:
+            pos_x -= TILE_SIZE
+            yield pos_x, pos_y
+        while pos_y < waypoint2[1]:
+            pos_y += TILE_SIZE
+            yield pos_x, pos_y
+        while pos_y > waypoint2[1]:
+            pos_y -= TILE_SIZE
+            yield pos_x, pos_y
+
+    def create_full_path(self):
+        """
+        Creates a looping path through all way points
+        and back to the starting position
+        """
+        self.path = []
+        self.pathidx = 0
+        for waypoint1, waypoint2 in zip([self.position] + self.waypoints,
+                                        self.waypoints + [self.position]):
+            for point in self.calculate_path(center_of_tile(*waypoint1),
+                                             center_of_tile(*waypoint2)):
+                self.path.append(center_of_tile(*point))
+
+    def move_along_path(self):
+        self.pathidx += 1 # TODO: Handle direction facing
+        self.pathidx %= len(self.path)
+        self.position = self.path[self.pathidx]
 
     def update(self):
-        self._move_randomly()
+        self.move_along_path()
+
+    def draw_path(self):
+        arcade.draw_line_strip(self.path + self.path[:1], self.pathcolor, 2)
