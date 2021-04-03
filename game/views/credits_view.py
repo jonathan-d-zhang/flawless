0af4e930f7d1532bbef606a615496f7099fb966e
EwwPhysics <74149971+EwwPhysics@users.noreply.github.com>
2021-04-03 09:54:58 -0400

    Info for user (#80)
    * Add more information to "How to Play" view
    
    Add information about accessing pause view.
    Reformat page so that all the information fits.
    
    * Add credits
    
    Now there are credits.
    They're all just bs but oh well
    Might change or add stuff later
    
    * remove pause screen information
    
    shifted pausing info to take its place.
    
    Co-authored-by: SavagePastaMan <69145546+SavagePastaMan@users.noreply.github.com>

--- game/views/credits_view.py
+++ game/views/credits_view.py
@@ -18,6 +18,19 @@ class CreditsView(MenuView):
         )
         self.draw_information_text(TEXT_COLOR, back=True, nav=True)
 
+        arcade.draw_text("Music", self.width // 4, self.height * .65, TEXT_COLOR, 18)
+        music_credits = "Music is original and created in GarageBand.\n\nAll loops Copyright 2021 Apple Inc."
+        arcade.draw_text(music_credits, self.width // 4, self.height * .54, TEXT_COLOR, 11)
+
+        arcade.draw_text("Sprites", self.width // 4, self.height * .45, TEXT_COLOR, 18)
+        sprites_credits = "Sprites were created by our team member eddpmett using Photoshop"
+        arcade.draw_text(sprites_credits, self.width // 4, self.height * .40, TEXT_COLOR, 11)
+
+        arcade.draw_text("Tilemaps", self.width // 4, self.height * .30, TEXT_COLOR, 18)
+        tilemaps_credits = "Tilemaps/levels were created by our team members using Tiled."
+        arcade.draw_text(tilemaps_credits, self.width // 4, self.height * .25, TEXT_COLOR, 11)
+
+
     def on_key_press(self, symbol, modifiers):
         if symbol == arcade.key.ESCAPE:
             self.switch_to("menu")
